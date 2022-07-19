0fbcb2c743def63ef473b9e5f885d590affb16e6
jinseong oh <jinseong@tmax.co.kr>
2022-07-19 16:28:16 +0900

    space-between & add toggle menu

--- src/home/Navigation.js
+++ src/home/Navigation.js
@@ -1,13 +1,18 @@
 import {Link} from "react-router-dom";
 import style from "./Navigation.module.css";
+import menu from "./menu.png";
 
 function NavMenu({menuText, linkTo}) {
-    return <div className={style.naviBox}>
+    return (<div className={style.naviBox}>
         <Link to={linkTo} className={style.naviText}>{menuText}</Link>
-    </div>
+    </div>);
 }
 
-function Navigation() {
+function ToggleNav() {
+    return <img src={menu} className={style.toggleNav} alt="logo" />;
+}
+
+function PlainNav() {
     return (
         <nav className={style.navigation}>
             <NavMenu linkTo="/" menuText="활동" />
@@ -15,6 +20,15 @@ function Navigation() {
             <NavMenu linkTo="/" menuText="오시는길" />
             <NavMenu linkTo="/" menuText="가나다라마" />
         </nav>
+    );
+}
+
+function Navigation() {
+    return (
+        <div>
+            <PlainNav />
+            <ToggleNav />
+        </div>
     );
 }
 
