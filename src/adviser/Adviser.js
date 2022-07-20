cd5243981805f50a896162b8bb48d1103de42be9
jinseong oh <jinseong@tmax.co.kr>
2022-07-20 14:56:48 +0900

    Add Adviser page

--- src/adviser/Adviser.js
+++ src/adviser/Adviser.js
@@ -1,8 +1,20 @@
 import Header from "../header/Header";
+import picture from "./pic.png";
+import style from "./Adviser.module.css";
 
 function Adviser() {
     return <div>
         <Header />
+        <div className={style.main__box}>
+            <img src={picture} className={style.picture} alt="logo"/>
+            <ul>
+                <p className={style.content}>약력</p>
+                <div className={style.list}>가나다마바사 아자차타가파하</div>
+                <div className={style.list}>가나다마바사 아자차타가파하</div>
+                <div className={style.list}>가나다마바사 아자차타가파하</div>
+                <div className={style.list}>가나다마바사 아자차타가파하</div>
+            </ul>
+        </div>
     </div>
 }
 
